c37f540c57bd46b0437efca7f41f83cb49bfb241
varun <varun.jajee@gmail.com>
2019-01-05 12:59:21 +0000

    Sxample 2

---
+++ src/main/java/org/var/spark/SpaEx2Anatomy.java
@@ -0,0 +1,13 @@
+package org.var.spark;
+
+import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaPairRDD;
+import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.api.java.JavaSparkContext;
+import scala.Tuple2;
+
+public class SpaEx2Anatomy {
+
+
+
+}
