717dd845f8b285dfc225cf8b0a1c3ea92811c9bc
varun <varun.jajee@gmail.com>
2019-01-06 08:33:15 +0000

    more resource file

---
+++ src/main/java/org/var/spark/SpaEx3RDD.java
@@ -0,0 +1,27 @@
+package org.var.spark;
+
+import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaPairRDD;
+import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.api.java.JavaSparkContext;
+import scala.Tuple2;
+
+public class SpaEx3RDD {
+    public static void main(String[] args) {
+        SparkConf sparkConf =
+                new SparkConf()
+                        .setMaster("local[*]")
+                        .setAppName("Counter")
+                        .set("spark.worker.cleanup.enabled", "true");
+
+        JavaSparkContext sparkContext = new JavaSparkContext(sparkConf);
+        String inputFilePath = "/Users/varunjajee/Data/spark/javaProgrammingGit/SparkProgramming/src/main/resources/word_count.txt";
+        int iPartitions = 3;
+        JavaRDD<String> inputFileRDDs;
+        inputFileRDDs = sparkContext.textFile(inputFilePath, iPartitions);
+        long count = inputFileRDDs.count();
+        System.out.println(count);
+    }
+
+
+}
