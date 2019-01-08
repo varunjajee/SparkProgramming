81676f467640a1d58987c36f2c5aa113498b38c2
varun <varun.jajee@gmail.com>
2019-01-08 00:03:21 +0000

    Add pait RDD example

--- src/main/java/org/var/spark/SpaEx3RDD.java
+++ src/main/java/org/var/spark/SpaEx3RDD.java
@@ -1,13 +1,21 @@
 package org.var.spark;
 
 import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaPairRDD;
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
+import scala.Tuple2;
 
 import java.util.Arrays;
 
 /*
-basic example of RDDs
+basic example of RDDs and pair RDDS
+
+1. MAP
+2. Flat MAP
+3. Mapto Pair.
+4. Reducebykey
+
 * */
 
 public class SpaEx3RDD {
@@ -30,6 +38,10 @@ public class SpaEx3RDD {
         inputFileRDDs.foreach(e->System.out.println(e));
 
         IntMapRDDTest(sparkContext);
+
+        FlatMap(inputFileRDDs);
+
+        PairRDD(inputFileRDDs);
     }
 
     private static void IntMapRDDTest(JavaSparkContext sparkContext)
@@ -41,5 +53,25 @@ public class SpaEx3RDD {
         intRDD.foreach(x -> System.out.println("map intRDD" +x));
         NewIntRDD.foreach(x -> System.out.println("map NewIntRDD" +x));
     }
+
+    private static void FlatMap(JavaRDD<String> inputFileRDDs)
+    {
+        JavaRDD<String> newFlatRDD = inputFileRDDs.flatMap(s -> Arrays.asList(s.split(",")).iterator());
+        newFlatRDD.foreach(x -> System.out.println("newFlatRDD" +x));
+    }
+
+    private static void PairRDD(JavaRDD<String> inputFileRDDs)
+    {
+        JavaRDD<String> newFlatRDD = inputFileRDDs.flatMap(s -> Arrays.asList(s.split(",")).iterator());
+        JavaPairRDD<String,Integer> mapToPairRDD = newFlatRDD.mapToPair(word -> new Tuple2<>(word,1));
+        mapToPairRDD.foreach(data -> System.out.println("Key= "+data._1() + "Value="+data._2()));
+        JavaPairRDD<String,Integer> reduceByKeyRDD = mapToPairRDD.reduceByKey((a,b)->a+b);
+        reduceByKeyRDD.foreach(data -> System.out.println("Key2= "+data._1() + " Value2= "+data._2()));
+
+    }
+
+
+
+
 
 }
